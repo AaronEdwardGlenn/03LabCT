56aed5b2fe5e04634b520deeef5e78f6c253c44a
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2019-12-04 15:20:07 -0800

    added first code block with mkdirp

---
+++ __tests__/fs-sys-func.test.js
@@ -0,0 +1,9 @@
+const { makeItDIRPdrop } = require('../lib/file-sys-funct');
+
+describe ('fs function that does stuff') = () => {
+    describe (makeItDIRPdrop), () => {
+        it('shoudl return a made directory and all parent directories'), () => {
+            expect(makeItDIRPdrop('./thebigpath/thelittlepath') => {
+r
+            }
+        }
