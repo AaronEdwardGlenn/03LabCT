2f3fc2c29db3bdbe41cd7b502f607c6602cbab62
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2019-12-04 17:24:59 -0800

    writeJSON and read JSON are complete

--- __tests__/fs-sys-func.test.js
+++ __tests__/fs-sys-func.test.js
@@ -1,9 +1,34 @@
-const { makeItDIRPdrop } = require('../lib/file-sys-funct');
+const fs = require('fs').promises;
+const { makeItDIRPdrop, writeJSON } = require('../lib/file-sys-funct');
 
-describe ('fs function that does stuff') = () => {
-    describe (makeItDIRPdrop), () => {
-        it('shoudl return a made directory and all parent directories'), () => {
-            expect(makeItDIRPdrop('./thebigpath/thelittlepath') => {
-r
-            }
-        }
+describe('fs functions', () => {
+    // describe('mkdirp', () => {
+    //     it('should create directory and all parent directories', () => {
+    //         return mkdirp('./this/is/it')
+    //         .then()
+    //         expect('yourMom');
+    //     });
+    // });
+    beforeAll(() => {
+        return makeItDIRPdrop('./it/got');
+    });
+            
+    describe('write JSON', () => {
+        it('should turn an object to JSON and write to a file', () => {
+            return writeJSON('./it/got/thing', {
+                cool: true,
+                num: 2
+            })
+                .then(() => {
+                    return fs.readFile('./it/got/thing', 'utf8');
+                })
+                .then(contents => {
+                    expect(JSON.parse(contents)).toEqual(({
+                        cool: true,
+                        num: 2
+                    }
+                    ));
+                });
+        });
+    });
+});
